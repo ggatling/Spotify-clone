de6257a9d6cbdf15453145ba96bfed7c453d5459
ggatling <gerda.gatling@gmail.com>
2019-09-30 19:01:56 -0400

    Added Song and UserRole models

--- src/main/java/com/example/spotifyclone/model/Song.java
+++ src/main/java/com/example/spotifyclone/model/Song.java
@@ -1,4 +1,46 @@
 package com.example.spotifyclone.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "songs")
 public class Song {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String songTitle;
+
+    @Column
+    private double songLength;
+
+    public Song (){}
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getSongTitle(){
+        return songTitle;
+    }
+
+    public void setSongTitle(){
+        this.songTitle = songTitle;
+    }
+
+    public double getSongLength(){
+        return songLength;
+    }
+
+    public void setSongLength(){
+        this.songLength = songLength;
+    }
+
 }
